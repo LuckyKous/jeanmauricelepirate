44e11fb0eb9e55965cdd95595ef30962478cde8f
LuckyKous <aecikus@gmail.com>
2019-05-19 22:17:13 +0200

    v1

---
+++ .gitignore/index.js
@@ -0,0 +1,16 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  bot.user.setGame("Les carottes c'est bon pour la santé !");
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '$help') {
+    msg.reply('Commandes :');
+    msg.reply('$help : Aide');
+  }
+});
+
+client.login(process.env.TOKENJMLP);
